30dcd6782200705669cf39167791f7035ec3f61e
feliciahsieh <felicia@tixwriteoff.com>
2017-07-17 00:52:27 +0000

    Frees structs as best as possible (for now)

--- currentInteractiveShell.c
+++ currentInteractiveShell.c
@@ -12,11 +12,11 @@ int interactiveShell(char **env, general_t *genHead)
 		bufferTokens = parseBuffer(buffer, genHead);
 		findBuiltin(genHead, bufferTokens[0]);
 		if (correctAbsPath(bufferTokens[0]))
-			createFork(bufferTokens);
+			createFork(bufferTokens, genHead);
 		else
 		{
 			bufferTokens[0] = findCmd(bufferTokens[0], genHead);
-			createFork(bufferTokens);
+			createFork(bufferTokens, genHead);
 		}
 	}
 	return (0);
