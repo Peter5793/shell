b036723de3f4dd7de53a0976b98baf4fb1bf474a
feliciahsieh <felicia@tixwriteoff.com>
2017-07-19 02:13:26 +0000

    betty

--- path.c
+++ path.c
@@ -1,5 +1,9 @@
 #include "shakeup.h"
-
+/**
+ * correctAbsPath - process command if correct info was entered by user
+ * @token: raw user input
+ * Return: Always 0.
+ */
 size_t correctAbsPath(char *token)
 {
 	struct stat st;
